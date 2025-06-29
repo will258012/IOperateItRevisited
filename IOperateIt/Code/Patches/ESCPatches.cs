ab58b978f2b7fd648f82c172958cb4c1a2885d06
Will <125041335+will258012@users.noreply.github.com>
2025-06-29 11:15:36 +0800

    支持使用ESC退出道路选择

---
+++ IOperateIt/Code/Patches/ESCPatches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using IOperateIt.Tools;
+using IOperateIt.UI;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace IOperateIt.Patches
+{
+    [HarmonyPatch]
+    [HarmonyAfter("Will258012.FPSCamera.Continued")]
+    internal class ESCPatches
+    {
+        private static readonly MethodBase[] TargetMethods = {
+            AccessTools.Method(typeof(GameKeyShortcuts), "Escape"),
+            AccessTools.Method(typeof(MapEditorKeyShortcuts), "Escape"),
+            AccessTools.Method(typeof(GameKeyShortcuts), "SteamEscape"),
+            AccessTools.Method(typeof(MapEditorKeyShortcuts), "SteamEscape"),
+        };
+        [HarmonyTargetMethods]
+        private static IEnumerable<MethodBase> TargetMethodsGetter() => TargetMethods;
+
+        [HarmonyPrefix]
+        private static bool HandleEscape()
+        {
+            if (MainPanel.Instance?.OnEsc() ?? false) return false;
+            if (ToolsModifierControl.GetCurrentTool<RoadSelectTool>() != null)
+            {
+                ToolsModifierControl.SetTool<DefaultTool>();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
